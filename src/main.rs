4f4ba1566f5b3f1a990dc8c265db8f5aa4a0910c
Tae Geun, Kim <edeftg@gmail.com>
2018-12-06 16:41:04 +0900

    Save & Log

--- src/main.rs
+++ src/main.rs
@@ -48,8 +48,36 @@ fn main() {
 
     let length = result.len();
     let mut result2: Vec<String> = Vec::new();
+    let mut i: usize = 0;
 
-    for i in 0 .. length/2-1 {
+    let mut ans3 = String::new();
+    println!("If you have temporary saved file? y/n");
+    stdin().read_line(&mut ans3).expect("Not correct answer");
+    ans3.retain(|x| x != '\n');
+
+    if ans3 == "y" {
+        let temp_file = Path::new("temp_1.txt");
+        let log_file = Path::new("log.txt");
+        let mut f = File::open(temp_file).expect("File not found");
+        let mut g = File::open(log_file).expect("File not found");
+        let mut temp = String::new();
+        let mut temp2 = String::new();
+
+        f.read_to_string(&mut temp)
+            .expect("Can't open this file");
+
+        g.read_to_string(&mut temp2)
+            .expect("Can't open this file");
+
+        result2 = temp.split('\n')
+            .map(|x| x.to_string()).collect();
+
+        temp2.retain(|x| x != '\n');
+
+        i = temp2.parse().unwrap();
+    }
+
+    while i < length / 2 - 1 {
         let x = &result[2*i];
         let y = &result[2*i+1];
         println!("{}  vs  {}", x, y);
@@ -59,13 +87,23 @@ fn main() {
 
         if ans == "1" {
             result2.push(x.to_string());
+            i += 1;
         } else if ans == "2" {
             result2.push(y.to_string());
+            i += 1;
         } else if ans == "3" {
             result2.push(x.to_string());
             result2.push(y.to_string());
+            i += 1;
+        } else if ans == "s" {
+            write_mp3_list("temp_1.txt", &result2);
+            let temp_write = format!("{}", i);
+            write_mp3_list("log.txt", &vec![temp_write]);
+            i += 1;
+        } else if ans == "4" {
+            i += 1;
         } else {
-            continue
+            continue;
         }
     }
 
